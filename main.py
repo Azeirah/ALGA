50c5e3f38e65b1368c415ebee18df7f45e8e7c25
martijn brekelmans <tijntje_7@msn.com>
2017-10-15 22:51:28 +0200

    Refactor out the adjacency matrix and nodes into own file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from AdjacencyMatrix import AdjacencyMatrix, Node
 
 
 dungeonConfig = {
@@ -6,50 +7,6 @@ dungeonConfig = {
     "X": 5,
     "Y": 5
 }
-
-
-class Node():
-    def __init__(self, ID):
-        self.ID = ID
-
-    def getID(self):
-        return self.ID
-
-    def __str__(self):
-        return str(self.ID)
-
-
-# adjency matrix info
-# http://www.algolist.net/Data_structures/Graph/Internal_representation
-class AdjacencyMatrix():
-    def __init__(self, amountOfNodes):
-        self.n = amountOfNodes
-        # initializes an empty array of size `n`
-        self.nodes = [None] * amountOfNodes
-
-        # fairly unreadable way to generate a 2d array in python
-        self.adjacency = list(
-            [list([0 for y in range(amountOfNodes)])
-                     for x in range(amountOfNodes)]
-        )
-
-    def __str__(self):
-        # x-axis
-        s = "\t" + "\t".join([str(node.getID()) for node in self.nodes]) + "\n"
-        for i in range(self.n):
-            # y-axis
-            row = str(i + 1)
-            for j in range(self.n):
-                row += "\t" + str(self.adjacency[j][i])
-            s += row + "\n"
-        return s
-
-    def addNode(self, x, node):
-        self.nodes[x] = node
-
-    def connectNodes(self, x, y):
-        self.adjacency[y][x] = 1
-        self.adjacency[x][y] = 1
 
 
 class UndirectedAcyclicGraph(AdjacencyMatrix):
